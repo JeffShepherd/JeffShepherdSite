558f74e8f07f0324ce02cf70201f09d993514931
JS <jeff.shep777@gmail.com>
2021-01-31 12:10:24 -0700

    Add functionality to move to corresponding page section on nav click

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+var nav = document.querySelector('.nav-container');
+var projectsSection = document.querySelector('.project-container');
+var aboutMeSection =  document.querySelector('.me-container');
 
 
+nav.addEventListener('click', function(event) {
+  var navSelection = event.target.closest('p')
 
+  if (navSelection.id === 'projects') {
+    projectsSection.scrollIntoView({behavior: 'smooth'});
+  } else if (navSelection.id === 'about') {
+    aboutMeSection.scrollIntoView({behavior: 'smooth'});
+  } else {
+    // contact form would pop here
+  }
+});
 
-document.getElementById("jump_to_this_location").scrollIntoView({behavior: 'smooth'});
