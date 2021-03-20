75ebab781d17c799e5589418cf9ddc2a9c8ab56d
JS <jeff.shep777@gmail.com>
2021-03-20 17:24:33 -0600

    Update landing page

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+var header = document.querySelector("header");
 var nav = document.querySelector('.nav-container');
 var welcomeSection = document.querySelector('.welcome-container');
 var projectsSection = document.querySelector('.project-container');
 var aboutMeSection = document.querySelector('.me-container');
 
 
+
+window.onscroll = function () {
+  stickHeader()
+};
+
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+//refactor this into a standalone function called in event listener
 nav.addEventListener('click', function (event) {
   var navSelection = event.target.closest('p')
 
@@ -22,6 +44,6 @@ nav.addEventListener('click', function (event) {
 
 window.addEventListener('load', function () {
   particlesJS.load('particles-js', './particles.json', function () {
-    console.log('callback - particles.js config loaded');
+    console.log('great success!');
   });
 })
